d729abc3d2fca0d9501a1ae1e7eaf1b547332909
Sergey Antonov <lenktongm@gmail.com>
2025-02-24 16:15:20 +0300

    refactor: add "dependent: :destroy" to associations to join tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :user_interests
+  has_many :user_interests, dependent: :destroy
   has_many :interests, through: :user_interests
-  has_many :user_skills
+  has_many :user_skills, dependent: :destroy
   has_many :skills, through: :user_skills
 
   validates :email, uniqueness: true
